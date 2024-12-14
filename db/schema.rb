4820b8954530c64c17969af816c44b530ab087a0
john <jiclatigay@gmail.com>
2024-12-14 21:48:28 +0800

    post like comment models and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_13_210811) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_14_134238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "follow_requests", force: :cascade do |t|
     t.bigint "follower_id"
@@ -23,6 +33,23 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_13_210811) do
     t.index ["followed_id"], name: "index_follow_requests_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_follow_requests_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_follow_requests_on_follower_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,6 +64,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_13_210811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "follow_requests", "users", column: "followed_id"
   add_foreign_key "follow_requests", "users", column: "follower_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
