147faaf75a5749b90abf6d9cafa552d44e6b4994
john <jiclatigay@gmail.com>
2024-12-14 23:35:59 +0800

    create posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
 end
