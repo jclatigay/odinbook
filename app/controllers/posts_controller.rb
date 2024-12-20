0dafe52b572db374b460d963c220c34f58373b9a
john <jiclatigay@gmail.com>
2024-12-20 16:38:13 +0800

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @posts = Post.all
   end
@@ -20,7 +22,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @post }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  resources :posts, only: [ :index, :show, :new, :create ] do
+  resources :posts do
     resources :comments, only: [ :create, :destroy ]
   end
 
